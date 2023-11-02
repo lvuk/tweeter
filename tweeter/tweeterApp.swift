54b75d6329560cd02b194af4d687e484b7653cc8
lvuk <luka.vuk456@gmail.com>
2023-11-02 17:06:39 +0100

    project setup

---
+++ tweeter/tweeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  tweeterApp.swift
+//  tweeter
+//
+//  Created by Luka Vuk on 02.11.2023..
+//
+
+import SwiftUI
+
+@main
+struct tweeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
